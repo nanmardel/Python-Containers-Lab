a5fb45272a7a80fa0a6104d57af6b4d07afd7ac7
nanmardel <martin_n1@denison.edu>
2022-04-20 19:10:55 -0500

    lab update

--- exersice-1.py
+++ exersice-1.py
@@ -1,4 +1,63 @@
-student = ["Marcos", "Mayra", "Tom", "Samantha"]
+#EXERSICE 1
+students = ["Marcos", "Mayra", "Tom", "Samantha"]
 
-print(Student[1])
-print(student[-1])
+print(students[1])
+print(students[-1])
+
+
+# EXERSICE 2
+
+foods = ('tacos', 'pizza', 'burger', 'burrito')
+
+print(foods)
+
+for f in foods:
+    print(f'{f} is a good food')
+
+# EXERSICE 3
+
+for f in foods:
+    print(foods[2:])
+
+# EXERSICE 4    
+
+home_town = {
+    'city': 'Chicago',
+    'state': 'Illinois',
+    'population': 271,
+}
+
+print(f"I was born in {home_town['city']},{home_town['state']}-population of {home_town['population']}.")
+
+# Exersice 5
+
+
+for key, val in home_town.items():
+    print(f"{key} = {val}")  
+
+# EXERSICE 6      
+
+
+cohort = []
+
+for index, student in enumerate(students):
+    cohort.append({
+        'student': student,
+        'fav_food': foods[index]
+    })
+
+for student in cohort:
+    print(student)
+
+#EXERSICE 7    
+
+awesome_students = [f'{name} is awesome!'for name in students]
+
+for students in  awesome_students:
+    print(student)
+
+# EXERSICE 8
+
+a_foods = [food for food in foods if "a" in food]
+print(a_foods)
+
